a7a4383ed03ea3965bffd911f8f673f394bf349e
Amir Rajabi <amirh_rajabi@yahoo.com>
2025-06-08 19:18:14 +0200

    llm changed

--- app.py
+++ app.py
@@ -7,17 +7,6 @@ from werkzeug.utils import secure_filename
 
 client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
 
-response = client.chat.completions.create(
-    model="deepseek-chat",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant"},
-        {"role": "user", "content": "Hello"},
-    ],
-    stream=False
-)
-
-print(response.choices[0].message.content)
-
 app = Flask(__name__)
 app.secret_key = '123'
 app.config['SESSION_TYPE'] = 'filesystem'
