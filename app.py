22ffc3c604626eb524802799cb242809b0209f43
Amir Rajabi <amirh_rajabi@yahoo.com>
2025-06-08 19:14:01 +0200

    llm changed

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import os
 from werkzeug.utils import secure_filename
 
 
-client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com/v1")
+client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
-    model="deepseek-reasoner",
+    model="deepseek-chat",
     messages=[
         {"role": "system", "content": "You are a helpful assistant"},
         {"role": "user", "content": "Hello"},
